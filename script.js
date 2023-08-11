ede6478d1ea5b103f116eff243c344711f848def
Caio Gomes <caiofgomesrj@gmail.com>
2023-08-11 11:29:31 -0300

    update projetos

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const githubLandingPage = () =>{
     let win = window.open(url,'_blank')
     win(focus)
 }
+
+const githubCadastro = () =>{
+    let url = 'https://caio-gomes2007.github.io/LoginAndCadastro.github.io/'
+    let win = window.open(url,'_blank')
+    win(focus)
+}
